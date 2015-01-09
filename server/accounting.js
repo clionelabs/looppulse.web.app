cd5033b9ab1e6dd20b5876fe24a116dd93a2def9
Vinci <vinci.cat@gmail.com>
2015-01-09 15:35:54 +0800

    minor refactor and preparation for routing

--- server/accounting.js
+++ server/accounting.js
@@ -1,23 +1,28 @@
 /*
- * User Account Manager
+ * User Account Manager, server side
  */
-Accounting = {
-  setup: function(settings) {
+_.extend(UserAccount, {
+  "ADMIN_GROUP": "__ADMIN__",
+  // Create admin user or give admin permission to specific account.
+  setupAdminUser: function(settings) {
     if (!settings || !settings.login) { return false; }
     // Admin Account Config
     var _userId = "";
     var _phrase = "";
     var _login = Meteor.settings.accounts.admin.login
     var _user = Meteor.users.findOne({ emails: { $elemMatch: { address:  _login } } })
+    var self = this;
 
-    if (_user && Roles.userIsInRole(_user, ['admin'])) {
-      console.info("[Init] Already have admin user, good to go.", _user)
-      return false;
+    if (_user && Roles.userIsInRole(_user, ['admin'], self.ADMIN_GROUP)) {
+      console.info("[UserAccount] Already have admin user, good to go.", _user)
+      return true;
+    } else {
+      console.info("[UserAccount] Admin user not found/without proper role")
     }
 
     // Setup a user if it doesn't exist
     if (!_user) {
-      console.info("[Init] Admin not found. Creating Account...")
+      console.info("[UserAccount] Admin not found. Creating Account...")
       // Set initial password or random string (when passphrase is null)
       // If random string is set in config, showInfoAfterCreation or mailInfoAfterCreation must have either one to be true
       _phrase = Meteor.settings.accounts.admin.passphrase || Math.random().toString(36).slice(-8);
@@ -26,59 +31,53 @@ Accounting = {
         'password': _phrase
       });
       if (Meteor.settings.accounts.admin.showInfoAfterCreation) {
-        console.info("[Init] New admin created:", _login, _phrase)
+        console.info("[UserAccount] New admin created:", _login, _phrase)
       }
       if (Meteor.settings.accounts.admin.mailInfoAfterCreation) {
         Accounts.sendEnrollmentEmail(_userId)
       }
+    } else {
+      _userId = _user._id;
+      console.info("[UserAccount] Admin user found: ", _userId)
     }
 
-    console.info("[Init] Adding Admin Role")
-    if (_user) {
-      Roles.addUsersToRoles(_user, ['admin']);
-    } else if (_userId) {
-      Roles.addUsersToRoles(_userId, ['admin']);
-    }
+    // Add admin permission
+    console.info("[UserAccount] Adding Admin Role")
+    Roles.addUsersToRoles(_userId, ['admin'], self.ADMIN_GROUP);
+    
     _login = null
     _phrase = null
 
   },
-  init: function() {
-
+  config: function() {
+    // Extra User Account Settings
     Accounts.validateNewUser(function(user) {
       var loggedInUser = Meteor.user();
 
       //@@TODO: configurable function access
-      if (Roles.userIsInRole(loggedInUser, ['admin'])) {
+      if (Roles.userIsInRole(loggedInUser, ['admin'], [UserAccount.ADMIN_GROUP])) {
         return true;
       }
 
       throw new Meteor.Error(403, "Not authorized to create new users");
     });
   },
-  start: function() {
+  startup: function() {
     var self = this;
-    Meteor.startup(function() {
-      var _settings = null;
-      console.info("User Account Config on Server side")
-      if(Meteor.settings && Meteor.settings.accounts) {
-        _settings = {};
-      } else {
-        console.info("[Init] No user accounts setting found. skip.")
-        return;
-      }
+    var _settings = null;
+    console.info("[Startup] User Account Config on Server side")
+    if(Meteor.settings && Meteor.settings.accounts) {
+      _settings = {};
+    } else {
+      console.info("[UserAccount] No user accounts setting found. skip.")
+      return;
+    }
 
-      // User Account Config
-      _settings.forbidClientAccountCreation = Meteor.settings.accounts.forbidClientAccountCreation || false;
+    //Meteor Account Setting transfer to `user-accounts`
 
-      // Setup
-      Accounts.config(_settings)
+    self.setupAdminUser(Meteor.settings.accounts.admin);
 
-      self.setup(Meteor.settings.accounts.admin);
-
-      self.init();
-
-    });
+    self.config();
 
   }
-};
+});
--- server/main.js
+++ server/main.js
@@ -4,6 +4,11 @@ Settings.load(Meteor.settings);
 // Start background processing
 Processing.start();
 
+
+Meteor.startup(function() {
+  UserAccount.startup();
+})
+
 // Reload fixtures
 if (Settings.loadFixtures) {
   Fixtures.reload();
