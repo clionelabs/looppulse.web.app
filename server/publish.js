0cdbd36474e4c3e519aa1d231c67aa1c0179bc34
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-01-09 15:36:29 +0800

    Added publish current workspace

--- server/publish.js
+++ server/publish.js
@@ -2,10 +2,23 @@
  * Here defines every publication.
  */
 
+/**
+ * Poi Metrics is return a self defined collection
+ */
 Meteor.publish('getPoisMetric', function (/*wsId*/) {
   //var workspace = Workspaces.findOne({_id : wsId});
   //TODO integrate with accounts related impl
   var workspace = Workspaces.findOne();
   var pois = Pois.find({ "wsId" : workspace._id}).fetch();
   return new PoisMetric({ "pois" : pois, "name" : workspace.poiName});
+});
+
+/**
+ * Workspace of the User
+ */
+Meteor.publish('getCurrentWorkspace', function(/*wsId*/) {
+  //var workspace = Workspaces.findOne({_id : wsId});
+  //TODO integrate with accounts related impl
+  var workspace = Workspaces.find(/*{ _id : wsId }*/);
+  return workspace;
 });
