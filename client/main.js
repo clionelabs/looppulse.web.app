8fe773a96ddc1bec27f3a692d26b5b435b65f98a
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-01-12 16:41:28 +0800

    Renamed start_up.js to main.js and remove Meteor.startup()

---
+++ client/main.js
@@ -0,0 +1,15 @@
+/**
+ * Here are some shim of functions to
+ */
+$.fn.toggleSVGClass = function(value) {
+    var currentClass = this.attr("class");
+    if (!currentClass.contains(value)) {
+        this.attr("class", currentClass + " " + value);
+    } else {
+        this.attr("class", currentClass.replace(value, ''));
+    }
+};
+
+$.fn.hasSVGClass = function(className) {
+    return new RegExp('(\\s|^)' + className + '(\\s|$)').test(this.attr('class'));
+};
