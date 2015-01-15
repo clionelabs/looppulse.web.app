7ccba61d63fe622a7df029b0042cae2ed1074a71
HiuKim <hiukim@hiukim.local>
2015-01-15 16:14:20 +0800

    Two factorings:
      - Some fixes on renaming Session to Capture
      - Move server side code of BeaconEvents collection to /server
    
    Conflicts:
    	lib/collections/beacon_events.js

---
+++ server/collections/beacon_events.js
@@ -0,0 +1,41 @@
+/**
+ *  Server extension on lib/collections/beacon_events.js
+ *
+ */
+
+/*
+ * Insert a beacon event given a firebase snapshot
+ *
+ * @param {Object} snapshot Firebase snapshot
+ **/
+BeaconEvents.insertFromFBSnapshot = function(snapshot) {
+  var key = snapshot.ref().toString().split(/\//).pop();
+
+  if (BeaconEvents.findOne({key: key})) {
+    console.warn("[BeaconEvents] event already inserted");
+    return;
+  }
+
+  var eventJSON = snapshot.val();
+  var visitorUUID = eventJSON.visitor_uuid;
+  var sessionId = eventJSON.session_id;
+  var type = eventJSON.type;
+  var captureId = eventJSON.capture_id;
+  var beacon = {
+    uuid: eventJSON.uuid,
+    major: eventJSON.major,
+    minor: eventJSON.minor
+  }
+  var createdAt = Date.parse(eventJSON.created_at);
+  var doc = {
+    key: key,
+    visitorUUID: visitorUUID,
+    sessionId: sessionId,
+    beacon: beacon,
+    type: type,
+    createdAt: createdAt,
+    captureId: captureId
+  };
+  console.log("[BeaconEvents] inserting doc: ", JSON.stringify(doc));
+  BeaconEvents.insert(doc);
+}
