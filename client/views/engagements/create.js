89228e8637185dc586f580454df46874d6a1d123
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-01-17 11:36:50 +0800

    Complete styling of poi selection

--- client/views/engagements/create.js
+++ client/views/engagements/create.js
@@ -1,3 +1,17 @@
-/**
- * Created by g on 15/1/15.
- */
+Template.engageCreate.helpers({
+   getSelectedPoi : function() {
+       //TODO impl
+       return { _id : "dfslghslje", name : "Jamie's Kitchen" , numInterested : 2300 };
+   },
+    setSelectedPoi : function(poi) {
+        //TODO impl to session
+    }
+
+});
+
+Template.engageCreate.destroyed = function() {
+
+    Session.clear(Template.engageCreate.selectedPoiSessionKey);
+}
+
+Template.engageCreate.selectedPoiSessionKey = "-engageCreate-selectedPoi";
