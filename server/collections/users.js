f65067130b868f3880f830dad4be1cbf2b5c32b8
Vinci <vinci.cat@gmail.com>
2015-01-22 15:22:21 +0800

    add the organizationId back

--- server/collections/users.js
+++ server/collections/users.js
@@ -1,4 +1,9 @@
 Meteor.startup(function(){
+    _.extend(Meteor.users, {
+    addOrganizationById: function (userId, organizationId) {
+      Meteor.users.update({_id: userId}, {$addToSet: {organizationIds: organizationId}});
+    }
+  });
   Meteor.users.find().observe({
     "added": function(newUser) {
       if (!newUser.password) {
