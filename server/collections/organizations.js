f6bfcd9107a1dd63d284b8a4aae6fb3a3c38ea4a
Vinci <vincicat@gmail.com>
2015-01-23 18:44:27 +0800

    set of user access control based on organisation

--- server/collections/organizations.js
+++ server/collections/organizations.js
@@ -1,10 +1,34 @@
+// Server side only operation
+
+// Security
+// Since user won't have userIds in client side most of the time,
+// so the client side security will be:
+//    "what you have is what you can access"
+// and the server side security will be query based on Id
 Organizations.addUserById = function (organizationId, userId) {
   return Organizations.update({_id: organizationId}, {$addToSet: {userIds: userId}});
 };
 
-Organizations.findByUserId = function (userId) {
-  return Organizations.find({userIds: {$in: [userId]}});
+// Wrapped find method require userid
+Organizations.findByUserId = function (userId, options) {
+  if (!userId) {
+    userId = ""
+  }
+
+  return Organizations.find({ userIds: {$in: [userId]} }, options);
 };
+
+Organizations.findById = function(organizationId, userId, options){
+  if (!userId) {
+    userId = ""
+  }
+
+  if (!organizationId) {
+    organizationId = ""
+  }
+
+  return Organizations.find({ _id: organizationId, userIds: {$in: [userId]} }, options);
+}
 
 Meteor.startup(function() {
   Meteor.users.find().observe({
