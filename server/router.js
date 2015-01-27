eb6141fd835079ba8158e3bcf6d31c89774ab643
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-01-27 20:11:39 +0800

    Refactor routing to 'modern' iron router

--- server/router.js
+++ server/router.js
@@ -1,31 +1,36 @@
-var isPostRequest = function(request, response) {
-  var requestMethod = request.method;
-  return requestMethod !== "POST";
-};
 
-Router.route('authenticate', {
-  path: '/api/authenticate/applications/:applicationId',
-  where: 'server',
-  action: function() {
-    if (!isPostRequest(this.request, this.response)) {
-      var requestMethod = request.method;
-      console.warn("[API] Unsupported method: " + requestMethod);
-      this.response.writeHead(405, {'Content-Type': 'text/html'});
-      this.response.end('<html><body>Unsupported method: ' + requestMethod + '</body></html>');
-      return;
-    }
+var unauthorized = function (route) {
+  if (route) { _.extend(this, route);}
+  var requestMethod = this.request.method;
+  console.warn("[API] Unsupported method: " + requestMethod);
+  this.response.writeHead(405, {'Content-Type': 'text/html'});
+  this.response.end('<html><body>Unsupported method: ' + requestMethod + '</body></html>');
+}
 
-    var token = this.request.headers["x-auth-token"];
-    var applicationId = this.params.applicationId;
-    var capture = this.request.body.capture;
-    var authenticatedResponse = Applications.authenticate(applicationId, token, capture);
-    if (authenticatedResponse.statusCode != 200) {
-      console.warn("[API] Application " + applicationId +
-                   " failed to authenticate with token " + token +
-                   " from " + JSON.stringify(this.request.headers));
+Router.route('/api/authenticate/applications/:applicationId',
+  {
+    where: 'server',
+    //use onBeforeAction for now as iron-router doesn't auto block undefined HTTP method
+    onBeforeAction: function() {
+      if (this.request.method !== 'POST') {
+        unauthorized(this);
+      }
     }
-    this.response.writeHead(authenticatedResponse.statusCode,
+  })
+    .post(function() {
+      var token = this.request.headers["x-auth-token"];
+      var applicationId = this.params.applicationId;
+      var capture = this.request.body.capture;
+      var authenticatedResponse = Applications.authenticate(applicationId, token, capture);
+      if (authenticatedResponse.statusCode != 200) {
+        console.warn("[API] Application " + applicationId +
+                      " failed to authenticate with token " + token +
+                      " from " + JSON.stringify(this.request.headers));
+      }
+      this.response.writeHead(authenticatedResponse.statusCode,
                                 {'Content-Type': 'application/json'});
-    this.response.end(JSON.stringify(authenticatedResponse));
-  }
-});
+      this.response.end(JSON.stringify(authenticatedResponse));
+    })
+  .get(unauthorized)
+  .put(unauthorized)
+  .delete(unauthorized);
