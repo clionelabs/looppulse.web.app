23b510147c9a67f0a5f350e9b9902f059f6df50c
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-01-27 19:14:34 +0800

    Unified style

--- client/views/commons/graphics_uploader.js
+++ client/views/commons/graphics_uploader.js
@@ -1,10 +1,4 @@
 Template.graphicsUploader.rendered = function() {
     var options = _.extend({}, Meteor.Dropzone.options, this.data);
-
-    if (this.data.id) {
-        this.dropzone = new Dropzone('#' + this.data.id + '.dropzone', options);
-    } else {
-       $(this.firstNode).dropzone(options);
-    }
-
+    $(this.firstNode).dropzone(options);
 };
