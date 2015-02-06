03b2b6bbb69309b1e0325ac65256bf9522ae6005
Thomas Pun <thomas@pun.io>
2015-02-06 15:48:33 +0800

    bulk create POIs from CSV #77

---
+++ server/external/csv.js
@@ -0,0 +1,39 @@
+CSV = {};
+
+CSV.bulkCreatePois = function (workspaceId, csvURL) {
+  var csvString = CSV.read(csvURL);
+  var results = Baby.parse(csvString, CSV.parseOptions);
+  var sanitized = CSV.sanitizePois(results.data);
+  return Pois.bulkCreate(workspaceId, sanitized);
+};
+
+CSV.parseOptions = {header: true, dynamicTyping: true, skipEmptyLines: true};
+
+CSV.read = function (csvURL) {
+  var data = HTTP.get(csvURL);
+  return data.content;
+};
+
+CSV.sanitizePois = function (unsanitizedPois) {
+  var pois = [];
+  _.each(unsanitizedPois, function (unsanitized) {
+    var poi = {};
+    _.each(Object.keys(unsanitized), function (badKey) {
+      var key = CSV.sanitizeKey(badKey);
+      poi[key] = unsanitized[badKey];
+    });
+    poi.beacon = { uuid: poi.uuid,
+                   major: parseInt(poi.major, 16),
+                   minor: parseInt(poi.minor, 16) };
+    delete poi.uuid;
+    delete poi.major;
+    delete poi.minor;
+    pois.push(poi);
+  });
+  return pois;
+};
+
+// CSV parser sometimes returns key with leading spaces
+CSV.sanitizeKey = function (unsanitizedKey) {
+  return unsanitizedKey.replace(/\s/g, '').toLowerCase();
+};
