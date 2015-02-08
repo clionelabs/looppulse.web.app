63b99e095d3ce1ef956deb5747c2bf11a7ea0e73
Thomas Pun <thomas@pun.io>
2015-02-08 22:13:20 +0800

    Organizations.import() creates everything needed under an org from the input json file.
    Server method to call JSON.importOrganizationsFromUrl() for creating
    seed organizations from console.

--- server/debug/fixtures.js
+++ server/debug/fixtures.js
@@ -21,51 +21,17 @@ Fixtures.clear = function() {
   Organizations.remove(fixtureSelector);
 };
 
-/**
- * Load fixture data from json file
- */
-Fixtures.load = function(orgFixture) {
-  var name = Fixtures.prefix + " " + orgFixture.name;
-
-  // Simply ignore the fixture if it has been imported before
-  // We do not support updating the fixtures at the moment - but you can always clear the old one first
-  if (Organizations.findOne({name: name})) {
-    console.log("[Fixture] Skipping Organization: ", orgFixture.name);
-    return;
-  }
-
-  console.log("[Fixture] Importing Organization: ", orgFixture.name);
-
-  var organizationId = Organizations.insert({name: name});
-
-  orgFixture.workspaces.forEach(function(wsFixture) {
-    var workspaceId = Workspaces.insert({organizationId: organizationId, name: wsFixture.name, poiDescriptors: wsFixture.poiDescriptors});
-
-    wsFixture.applications.forEach(function(appFixture) {
-      Applications.insert({workspaceId: workspaceId, name: appFixture.name, token: appFixture.token});
-    });
-
-    wsFixture.pois.forEach(function(poiFixture) {
-      Pois.insert({workspaceId: workspaceId, name: poiFixture.name, beacon: poiFixture.beacon});
-    });
-
-    wsFixture.geofences.forEach(function(geofenceFixture) {
-      Geofences.insert({workspaceId: workspaceId, lat: geofenceFixture.lat, lng: geofenceFixture.lng, radius: geofenceFixture.radius});
-    });
-  });
-
-  orgFixture.users.forEach(function(userFixture) {
-    var userId = Accounts.createUser(userFixture);
-    Organizations.addUserById(organizationId, userId);
-  });
-};
-
 Fixtures.loadFromDefault = function() {
   var fixture = JSON.parse(Assets.getText("fixtures.json"));
   _.each(fixture.organizations, function(orgFixture) {
-    Fixtures.load(orgFixture);
+    orgFixture.name = Fixtures.applyPrefix(orgFixture.name);
+    Organizations.import(orgFixture);
   });
 };
+
+Fixtures.applyPrefix = function (name) {
+  return Fixtures.prefix + " " + name;
+}
 
 Fixtures.toLoad = function() {
   return (Meteor.settings.DEBUG &&
