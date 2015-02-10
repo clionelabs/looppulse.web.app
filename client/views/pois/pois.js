0176dd2752bc43daeff3f91222b435622dcb2c24
HiuKim <hiukim@hiukim.local>
2015-02-11 04:46:04 +0800

    Hook up POIS export button

--- client/views/pois/pois.js
+++ client/views/pois/pois.js
@@ -34,5 +34,32 @@ Template.pois.swipe = function(isActive) {
     }
 };
 
+Template.pois.events({
+  "click #gauge-download": function (e, tmpl) {
+    // TODO: get workspaceId
+    var workspaceId = "BtpqiPgu7xzC8PHdb";
+    Meteor.call('exportPois', workspaceId, function(error, result) {
+      if (error) {
+        Notifications.error('Export CSV', 'Export CSV failed -- ' + error + ' --');
+      } else {
+        var headings = true;
+        var quotes = true;
+        var uri = "data:text/csv;charset=utf-8," + escape(result);
+        var filename =  workspaceId + "-" + "pois" +  "-" + moment().format() + ".csv";
 
-
+        // TODO: put the following lines into a utility function
+        // window.open has ugly filename. use this hacky method to allow customizing filename
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+          document.body.appendChild(link); // Firefox requires the link to be in the body
+          link.download = filename;
+          link.href = uri;
+          link.click();
+          document.body.removeChild(link); // remove the link when done
+        } else {
+          location.replace(uri);
+        }
+      }
+    });
+  }
+});
