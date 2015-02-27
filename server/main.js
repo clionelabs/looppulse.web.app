9dbfc8e839f80775faa7943e7adb2bb9dee0d0b2
hiukim <hiukim@gmail.com>
2015-02-28 00:51:02 +0800

    Remove unused code

--- server/main.js
+++ server/main.js
@@ -3,16 +3,3 @@ System.configure();
 
 // Start background processing
 Processing.start();
-
-// Re-compute Interests
-if (true) {
-  Journeys.find().forEach(function(journey) {
-    var poiId = journey.poiId;
-    var visitorUUID = journey.visitorUUID;
-    if (PoiInterests.isInterested(journey)) {
-      PoiInterests.markInterested(poiId, visitorUUID);
-    } else {
-       PoiInterests.markNotInterested(poiId, visitorUUID);
-    }
-  });
-}
